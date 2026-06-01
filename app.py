46e188294d2a65e939feeb939b24b11e0e9d058b
nicole634 <escobarcapalot@gmail.com>
2026-06-01 07:31:35 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/greet", methods=["POST"])
+def greet():
+    name = request.json.get("name")
+    return jsonify({"message": f"Hello, {name}! 👋"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
